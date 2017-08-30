7db17825155b9d6fb2538a9616b9371810184cbe
John Sylvain <john.n.sylvain@gmail.com>
2017-08-30 17:08:52 -0500

    added routes

--- src/shared/app.jsx
+++ src/shared/app.jsx
@@ -8,9 +8,11 @@ import Helmet from 'react-helmet'
 import { APP_NAME } from './config'
 import Nav from './components/nav'
 import HomePage from './components/page/home'
-import HelloPage from './components/page/hello'
-import HelloAsyncPage from './components/page/hello-async'
+// import HelloPage from './components/page/hello'
+// import HelloAsyncPage from './components/page/hello-async'
 import NotFoundPage from './components/page/not-found'
+import RacePage from './components/page/race'
+
 import {
   HOME_PAGE_ROUTE,
   HELLO_PAGE_ROUTE,
@@ -23,8 +25,7 @@ const App = () =>
     <Nav />
     <Switch>
       <Route exact path={HOME_PAGE_ROUTE} render={() => <HomePage/>} />
-      <Route path={HELLO_PAGE_ROUTE} render={() => <HelloPage/>} />
-      <Route path={HELLO_PAGE_ASYNC_ROUTE} render={() => <HelloAsyncPage/>} />
+      <Route path={RACE_ROUTE} render={() => <RacePage/>} />
       <Route component={NotFoundPage} />
     </Switch>
   </div>
